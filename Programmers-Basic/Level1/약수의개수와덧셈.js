f48cb9efe09d5db635bc6b4bb74f32eb8b63bb44
Yeonseo-Jo <jys073190@gmail.com>
2023-10-03 12:12:36 +0900

    level1 - 약수의개수와덧셈

---
+++ Programmers-Basic/Level1/약수의개수와덧셈.js
@@ -0,0 +1,30 @@
+//solution
+
+function solution(left, right) {
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
+    }
+    if (count % 2) answer -= i;
+    else answer += i;
+  }
+
+  return answer;
+}
+
+//다른 풀이
+//약수를 제곱근으로 구하기 - 제곱근이 정수이면 약수의 개수가 홀수이다!!
+// function solution(left, right) {
+//     var answer = 0;
+//     for (let i = left; i <= right; i++) {
+//         if (Number.isInteger(Math.sqrt(i))) {
+//             answer -= i;
+//         } else {
+//             answer += i;
+//         }
+//     }
+//     return answer;
+// }
