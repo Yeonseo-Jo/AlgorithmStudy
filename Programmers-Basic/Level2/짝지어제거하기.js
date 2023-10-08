ab68dd1d941f8aeb57470b9154bfdeb76710374c
Yeonseo-Jo <jys073190@gmail.com>
2023-10-09 02:56:01 +0900

    level 2 - 짝지어 제거하기.js

---
+++ Programmers-Basic/Level2/짝지어제거하기.js
@@ -0,0 +1,14 @@
+//solution
+
+function solution(s) {
+  const array = s.split("");
+  const stack = [];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    array[i] === stack.at(-1) ? stack.pop() : stack.push(array[i]);
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
+
+// 다른 풀이
